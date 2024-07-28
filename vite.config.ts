d10a7b900895874bf71c97e758a3e74ff58c9913
Icaro Albar <icaro.albar@live.com>
2024-07-27 22:26:37 -0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import path from "path"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      'src': path.resolve(__dirname, './src'),
+      src: path.resolve(__dirname, "./src"),
     },
   },
   define: {
-    __APP_ENV__: process.env.VITE_VERCEL_ENV,
+    __VITE_FIND_ALL_DATA__: process.env.VITE_FIND_ALL_DATA,
+    __VITE_LOGIN_USER__: process.env.VITE_LOGIN_USER,
   },
-})
+});
